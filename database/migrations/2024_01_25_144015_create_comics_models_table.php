8c357014b049fa6fdd7b8d5e3c7ae0b3790f0a28
FrancescoBonanno1 <francescosoapbonanno@gmail.com>
2024-01-25 16:26:32 +0100

    milestone 1 completata

--- database/migrations/2024_01_25_144015_create_comics_models_table.php
+++ database/migrations/2024_01_25_144015_create_comics_models_table.php
@@ -12,13 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('Comics', function (Blueprint $tabella){
-            $tabella->char('title', 100);
-            $tabella->char('description',255);
-            $tabella->char('thumb',255);
-            $tabella->dateTime('price');
-            $tabella->dateTime('series');
-            $tabella->string('sale_date',10);
-            $tabella->integer('type', 7);
+            $tabella->id();
+            $tabella->text('title');
+            $tabella->text('description');
+            $tabella->string('thumb',255);
+            $tabella->decimal('price');
+            $tabella->char('series', 100);
+            $tabella->date('sale_date');
+            $tabella->char('type', 100);
             $tabella->timestamps();
            
            
