b21fd0f64a6ffdb7cf28b8232ad5a89fb48c9ffd
FrancescoBonanno1 <francescosoapbonanno@gmail.com>
2024-01-25 17:30:24 +0100

    seed impostato ma errore sul popolamento

--- database/seeders/ComicsSeeder.php
+++ database/seeders/ComicsSeeder.php
@@ -16,6 +16,14 @@ class ComicsSeeder extends Seeder
         $comics_array=config("comic");
         foreach($comics_array as $comic_item){
             $new_comic= new comic();
+            $new_comic->titolo = $comic_item["title"];
+            $new_comic->descrizione = $comic_item["description"];
+            $new_comic->img = $comic_item["thumb"];
+            $new_comic->prezzo = $comic_item["price"];
+            $new_comic->serie = $comic_item["series"];
+            $new_comic->data_vendita = $comic_item["sale_date"];
+            $new_comic->tipo = $comic_item["type"];
+            $new_comic->save();
 
         }
     }
