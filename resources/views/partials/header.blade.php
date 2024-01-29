5020c8d411a5cf55201362ac08df539827221d4c
FrancescoBonanno1 <francescosoapbonanno@gmail.com>
2024-01-29 13:26:29 +0100

    aggiornamento componente header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,28 @@
+
 <header>
-    <a href="{{ route('home') }}">Home</a>
+    <div id="bluelabel">
+        <span id="fixed1">DC POWER VISA</span>
+        <span id="fixed2">Additional Dc Sites</span>
+    </div>
+    <div id="trueheader">
+        <div id="img_container">
+            <img src="public\images\dc-logo-bg.png" alt="logo">
+        </div>
+
+        <div id="link_container">
+            <a href="Characters">Characters</a>
+            <a href="Comics">Comics</a>
+            <a href="Movies">Movies</a>
+            <a href="TV">TV</a>
+            <a href="Games">Games</a>
+            <a href="Collectibles">Collectibles</a>
+            <a href="Videos">Videos</a>
+            <a href="Fans">Fans</a>
+            <a href="News">News</a>
+            <a href="Shop">Shop</a>
+        </div>
+        <div id="Searchbar_cont">
+            <input type="search">
+        </div>
+    </div>
 </header>
